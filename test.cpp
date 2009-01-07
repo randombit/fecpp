f0b65e9a25000ce83a9299adab93a82937585905
Jack Lloyd <lloyd@randombit.net>
2009-01-07 20:22:50 +0000

    Add recovery/decoder tests
    Monotone-Parent: 45a942d038fccf312f15c9d54ed74fe7c27ba826
    Monotone-Revision: 62cf977855d1f91566322fa018c17d61390ca950

--- test.cpp
+++ test.cpp
@@ -1,32 +1,71 @@
 #include "fecpp.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void gen_test_vector(int k, int n)
+   {
+   fec_parms* code = fec_new(k, n);
+
+
+
+   unsigned int buf_size = k * 4;
+   unsigned char* buf = new byte[buf_size];
+
+   for(int i = 0; i != buf_size; ++i)
+      buf[i] = rand();
+
+   printf("k=%d, n=%d, input=", k, n);
+   for(int i = 0; i != buf_size; ++i)
+      printf("%02X", buf[i]);
+   printf(", ");
+
+   unsigned char* buf_ptrs[k];
+
+   for(int i = 0; i != k; ++i)
+      {
+      buf_ptrs[i] = buf + i * (buf_size / k);
+      /*
+      printf("BUF %d: ", i);
+      for(int l = 0; l != buf_size / k; ++l)
+         printf("%02X", buf_ptrs[i][l]);
+      printf("\n");
+      */
+      }
+
+   unsigned char* fec = new byte[buf_size / k];
+
+   for(int i = 0; i != n; ++i)
+      {
+      fec_encode(code, buf_ptrs, fec, i, buf_size / k);
+
+      printf("block_%d=", i);
+      for(int j = 0; j != buf_size / k; ++j)
+         printf("%02X", fec[j]);
+      if(i != n -1)
+         printf(", ");
+      }
+
+   printf("\n");
+
+   fec_free(code);
+
+   }
 
 int main()
    {
-   fec_parms* code = fec_new(4, 8);
+   srand(0);
 
-   unsigned char buf[64] = { 0 };
-
-   for(int i = 0; i != sizeof(buf); ++i)
-      buf[i] = i * 13;
-
-   unsigned char* buf_ptrs[4];
-
-   for(int i = 0; i != 4; ++i)
-      buf_ptrs[i] = buf + i * (sizeof(buf) / 4);
-
-   unsigned char fec[sizeof(buf) / 4];
-
-   for(int i = 0; i != 8; ++i)
+   for(int n = 1; n != 10; ++n)
       {
-      fec_encode(code, buf_ptrs, fec, i, sizeof(buf) / 4);
+      for(int k = 1; k != 10; ++k)
+         {
+         if(k >= n)
+            continue;
 
-      for(int j = 0; j != sizeof(fec); ++j)
-         printf("%02X", fec[j]);
-      printf("\n");
+         gen_test_vector(k, n);
+         }
       }
-
-   fec_free(code);
 
    return 0;
    }
